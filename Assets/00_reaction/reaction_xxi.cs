43a2d3c24df924d8702da9a9bae33611cf6c7ead
droqen <hi@droqen.com>
2019-10-08 23:52:21 -0400

    Initial commit

---
+++ Assets/00_reaction/reaction_xxi.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reaction_xxi : navdi3.xxi.BaseTilemapXXI
+{
+    private void Start()
+    {
+        InitializeManualTT();
+    }
+
+    public override int[] GetSolidTileIds()
+    {
+        return new int[] { };
+    }
+    public override int[] GetSpawnTileIds()
+    {
+        return new int[0];
+    }
+    public override void SpawnTileId(int TileId, Vector3Int TilePos)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# 2019-10-Pressure
+ Exploring TIME PRESSURE
